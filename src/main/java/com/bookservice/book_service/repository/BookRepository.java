70b30adf5710fd3e97386f457072734b0b95a378
GustavoZanardi15 <ulianzanardig@gmail.com>
2025-06-03 11:34:02 -0300

    feat: criar BookRepository extendendo JpaRepository

--- src/main/java/com/bookservice/book_service/repository/BookRepository.java
+++ src/main/java/com/bookservice/book_service/repository/BookRepository.java
@@ -1,4 +1,10 @@
 package com.bookservice.book_service.repository;
 
-public class BookRepository {
+import com.bookservice.book_service.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+
 }
