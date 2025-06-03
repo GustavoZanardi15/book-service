c9042dc9c2dd75171338edcb8f2a0d74396c1f87
GustavoZanardi15 <ulianzanardig@gmail.com>
2025-06-03 11:35:41 -0300

    feat: criar BookController com endpoints REST para CRUD e atualização de status

--- src/main/java/com/bookservice/book_service/controller/BookController.java
+++ src/main/java/com/bookservice/book_service/controller/BookController.java
@@ -1,4 +1,49 @@
 package com.bookservice.book_service.controller;
 
+import com.bookservice.book_service.model.Book;
+import com.bookservice.book_service.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
 public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @PostMapping
+    public ResponseEntity<Book> createBook(@RequestBody Book book) {
+        Book created = bookService.createBook(book);
+        return ResponseEntity.ok(created);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Book>> getAllBooks() {
+        return ResponseEntity.ok(bookService.getAllBooks());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable Long id) {
+        return bookService.getBookById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book book) {
+        return bookService.updateBook(id, book)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PatchMapping("/{id}/status")
+    public ResponseEntity<Book> updateBookStatus(@PathVariable Long id, @RequestBody String status) {
+        return bookService.updateBookStatus(id, status)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
